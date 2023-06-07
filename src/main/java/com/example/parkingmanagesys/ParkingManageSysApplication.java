ba35a792cf80e9cdb741312263c82ee23ed3f02c
冀云龙 <1596118915@qq.com>
2023-06-07 09:53:36 +0800

    1.0

---
+++ src/main/java/com/example/parkingmanagesys/ParkingManageSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManageSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingManageSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parkingmanagesys/ParkingManageSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManageSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
