519fef1a6b07612ba280cbdfe5ee620ec366af22
B20022135熊海锋 <931234498@qq.com>
2023-06-12 09:51:28 +0800

    用户的service

---
+++ src/main/java/com/example/parkingmanagesys/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package com.example.parkingmanagesys.Service.Impl;
+
+import com.example.parkingmanagesys.DAO.BookingInformationMapper;
+import com.example.parkingmanagesys.DAO.BookingMapper;
+import com.example.parkingmanagesys.DAO.ParkingSpaceMapper;
+import com.example.parkingmanagesys.DAO.UsersMapper;
+import com.example.parkingmanagesys.Pojo.Booking;
+import com.example.parkingmanagesys.Pojo.BookingInformation;
+import com.example.parkingmanagesys.Pojo.ParkingSpace;
+import com.example.parkingmanagesys.Pojo.User;
+import com.example.parkingmanagesys.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+@Autowired
+private UsersMapper usersMapper;
+@Autowired
+private BookingInformationMapper bookingInformationMapper;
+@Autowired
+private BookingMapper bookingMapper;
+@Autowired
+private ParkingSpaceMapper parkingSpaceMapper;
+
+    @Override
+    public boolean updateByCarId(User user) {
+        return usersMapper.updateByCarId(user);
+    }
+
+    @Override
+    public boolean deleteByCarId(String carId) {
+        return usersMapper.deleteByCarId(carId);
+    }
+
+    @Override
+    public BookingInformation selectBookingInformationByCarId(String carId) {
+        return bookingInformationMapper.selectByBookingId(bookingMapper.selectByCarId(carId).getBookingId());
+    }
+
+    @Override
+    public boolean updateBookingInformationByCarId(String carId,String date) throws ParseException {
+        //查所有的车位，找到空闲车位，改为“预定”,得到spaceId
+        ParkingSpace parkingSpace=new ParkingSpace();
+        Integer spaceId;
+        List<ParkingSpace> parkingSpaceList=parkingSpaceMapper.selectAll();
+        for(int i=0;i<parkingSpaceList.size();i++){
+            if(parkingSpaceList.get(i).getSpaceState()=="空闲")
+            {
+                parkingSpace=parkingSpaceList.get(i);
+                break;
+            }
+        }
+        spaceId=parkingSpace.getSpaceId();
+        parkingSpace.setSpaceState("预定");
+        parkingSpaceMapper.updateBySpaceId(parkingSpace);
+        //添加bookingInformation信息
+        BookingInformation bookingInformation=new BookingInformation();
+        List<BookingInformation> bookingInformationList=bookingInformationMapper.selectAll();
+        bookingInformation.setBookingId(bookingInformationList.size());
+        //将String的日期改为正常日期，如果不对return false
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm");
+        bookingInformation.setBookingTime(ft.parse(date));
+        bookingInformationMapper.insertBookingTime(bookingInformation);
+        //添加booking信息
+
+        return false;
+    }
+}
---
+++ src/main/java/com/example/parkingmanagesys/Service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.parkingmanagesys.Service;
+
+import com.example.parkingmanagesys.Pojo.BookingInformation;
+import com.example.parkingmanagesys.Pojo.User;
+
+public interface UserService {
+    //修改用户个人信息
+    public boolean updateByCarId(User user);
+    //用户注销
+    public boolean deleteByCarId(String carId);
+    //查看预定
+    public BookingInformation selectBookingInformationByCarId(String carId);
+    //预定
+    public boolean updateBookingInformationByCarId(String carId);
+}
